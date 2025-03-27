d9fc814f7d74444fc3afc38227fa9daadc0183f6
ispik <ispik6441@proton.me>
2025-03-27 18:09:22 +0300

    start gh-pages

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+        title: {
+            "en": "Khaxy Documentation",
+            "tr": "Khaxy Belgeleri"
+        },
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        sidebar: [
+            {
+                label: "Resources",
+                translations: {
+                    "tr": "Kaynaklar"
+                },
+                items: [
+                    { label: "Terms of Service", slug: "resources/tos", translations: {tr: "Hizmet Şartları"}},
+                    {label: "Privacy Policy", slug: "resources/privacy", translations: {tr: "Gizlilik Politikası"}}
+                ]
+            }
+        ],
+        defaultLocale: 'root',
+        locales: {
+            root: {
+                label: 'English',
+                lang: 'en',
+            },
+            tr: {
+                label: 'Türkçe',
+                lang: 'tr',
+            }
+        },
+		})],
+});
