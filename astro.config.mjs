913d14555c2185431373fb06cc2a9eb28ab9f5ac
ispik <ispik6441@proton.me>
2025-05-07 19:25:39 +0300

    Polishing the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,8 +13,13 @@ export default defineConfig({
             "en": "Khaxy Documentation",
             "tr": "Khaxy Belgeleri"
         },
+        favicon: "./public/favicon.svg",
+        logo: {
+            light: './src/assets/khaxy-mini-logo-black.svg',
+            dark: './src/assets/khaxy-mini-logo-white.svg',
+        },
         social: {
-            github: 'https://github.com/withastro/starlight',
+            github: 'https://github.com/Arsabutispik/KhaxyRewrite',
         },
         sidebar: [
             {
@@ -25,6 +30,20 @@ export default defineConfig({
                 items: [
                     { label: "Terms of Service", slug: "resources/tos", translations: {tr: "Hizmet Şartları"}},
                     {label: "Privacy Policy", slug: "resources/privacy", translations: {tr: "Gizlilik Politikası"}}
+                ]
+            },
+            {
+                label: "Configuration",
+                translations: {
+                    "tr": "Yapılandırma"
+                },
+                items: [
+                    {label: "Variables", slug: "configuration/variables", translations: {tr: "Değişkenler"}},
+                    {label: "Register Settings", slug: "configuration/register-settings", translations: {tr: "Kayıt Ayarları"}},
+                    {label: "Welcome & Leave Settings", slug: "configuration/welcome-leave-settings", translations: {tr: "Giriş & Çıkış Ayarları"} },
+                    {label: "Moderation Settings", slug: "configuration/moderation-settings", translations: {tr: "Moderasyon Ayarları"}},
+                    {label: "Role Settings", slug: "configuration/role-settings", translations: {tr: "Rol Ayarları"}},
+                    {label: "Miscellaneous Settings", slug: "configuration/miscellaneous-settings", translations: {tr: "Çeşitli Ayarlar"}},
                 ]
             }
         ],
